6e2d83143d9dfd5e117b31270dac285064d10fad
Maksym H. <Maksym48@pm.me>
2025-02-02 20:26:06 +0100

    Try to improve HUD

--- hud.lua
+++ hud.lua
@@ -5,15 +5,15 @@ core.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
 	local data = {}
 
-	data.imageid = player:hud_add({
+	--[[data.imageid = player:hud_add({
 		hud_elem_type = "image",
 		name = "MailIcon",
-		position = {x = 0.475, y = 1},
+		position = {x = 0.5, y = 1},
 		text = "",
 		scale = {x=1,y=1},
 		alignment = {x = 0, y = -1},
-		offset = {x = 0, y = -130},
-	})
+		offset = {x = -20, y = -130},
+	})]]
 
 	data.textid = player:hud_add({
 		hud_elem_type = "text",
@@ -23,7 +23,7 @@ core.register_on_joinplayer(function(player)
 		number = 0xFFFFFF,
 		scale = {x=1,y=1},
 		alignment = {x = 0, y = -1},
-		offset = {x = 0, y = -130},
+		offset = {x = 0, y = -130}, -- x = -20
 	})
 
 
@@ -52,11 +52,10 @@ function mail.hud_update(playername, messages)
 	end
 
 	if unreadcount == 0 or (not mail.get_setting(playername, "hud_notifications")) then
-		player:hud_change(data.imageid, "text", "")
+	--	player:hud_change(data.imageid, "text", "")
 		player:hud_change(data.textid, "text", "")
 	else
-		player:hud_change(data.imageid, "text", "email_mail.png")
-		player:hud_change(data.textid, "text", unreadcount .. "\032\032\032/mail")
+	--	player:hud_change(data.imageid, "text", "email_mail.png")
+		player:hud_change(data.textid, "text", "✉️" .. unreadcount .. "\032\032/mail")
 	end
-
 end
